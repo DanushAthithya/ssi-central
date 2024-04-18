3859980d572972d58a0831483e1971ac45a39e75
DanushAthithyaRK <danush@gmail.com>
2024-04-18 14:35:32 +0530

    added ssi-management microservice

---
+++ ssi-management/src/main/java/com/stackroute/ssimanagement/SsiManagementApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.ssimanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiManagementApplication.class, args);
+	}
+
+}
