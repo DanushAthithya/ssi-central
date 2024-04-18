278692f5886f191dbe5b16d431ecbc212b03d55d
DanushAthithyaRK <danush@gmail.com>
2024-04-18 17:10:50 +0530

    added userAdd service in user-service

--- user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
@@ -1,13 +1,27 @@
 package com.stackroute.userservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.stackroute.userservice.model.User;
+import com.stackroute.userservice.repository.UserRepository;
 
 public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserRepository userrepo;
 
 	@Override
 	public boolean addUser(User user) {
 		// TODO Auto-generated method stub
-		return false;
+		if(userrepo.existsById(user.getEmpId()))
+		{
+			return false;
+		}
+		else {
+			String password=this.passwordGenerator();
+			user.setPassword(this.encryptPassword(password));
+			userrepo.save(user);
+			return true;
+		}
 	}
 
 	@Override
