d4f2f9f614b83d78afe95d1f7ce3c4adc998605e
DanushAthithyaRK <danush@gmail.com>
2024-04-19 12:56:02 +0530

    added encryptPassword Service

--- user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
@@ -95,7 +95,28 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public String encryptPassword(String password) {
 		// TODO Auto-generated method stub
-		return null;
+		try {
+			// Create MessageDigest instance for SHA
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+			// Add password bytes to digest
+			md.update(password.getBytes());
+
+			// Get the hash's bytes
+			byte[] bytes = md.digest();
+
+			// Convert bytes to hexadecimal format
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < bytes.length; i++) {
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+
+			// Get complete hashed password in hexadecimal format
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	@Override
