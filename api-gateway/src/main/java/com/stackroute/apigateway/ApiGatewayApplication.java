08ed2d21c594c5cbadadf49d8f9ffbda258f0a4c
riya <riyajoseph179@gmail.com>
2024-05-06 07:38:48 +0530

    eureka

--- api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
@@ -2,13 +2,26 @@ package com.stackroute.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		
+		return builder.routes()
+				.route(r->r.path("/authorization/**").uri("lb://AUTHORIZATION"))
+				.route(r->r.path("/user-service/**").uri("lb://USER_SERVICE"))
+				.route(r->r.path("/productwebapp/**").uri("lb://PRODUCT_WEBAPP"))
+				.route(r->r.path("/ssi-management/**").uri("lb://SSI_MANAGEMENT"))
+				.build();
 	}
 
 }
